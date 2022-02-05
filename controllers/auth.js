47bf8c83548f014ac722c71f9aa44809f76ee711
Ham <infowamys@gmail.com>
2022-02-05 18:58:57 +0400

    add the bearer in token

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,7 +19,7 @@ module.exports.login = async function (req, res) {
             }, keys.jwt, {expiresIn: 60 * 60});
 
             res.status(200).json({
-                token: token
+                token: `Bearer ${token}`
             })
         }else{
             //passer@ chi hamnknum
